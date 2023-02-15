3dc718282ace0abfdf503dcf047923207ba5b54d
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-15 20:19:58 +0300

    commit tree c5d3461ca78456eff4bdb8abf2f77ef541df507c

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+ 
+ INSERT INTO  bears(name, age, sex, color, temperament, alive)
+ VALUES ('Mr. Chocolate' ,5, 'M', 'brown', 'melancholic', TRUE),
+   ('Rowdy' ,4, 'M', 'black', 'aggressive', TRUE),
+   ('Tabitha' ,3, 'F', 'brown', 'social', TRUE),
+   ('Sergeant Brown' ,4, 'M', 'black', 'phlegmatic', TRUE),
+   ('Melissa' ,14, 'F', 'white', 'melancholic', TRUE),
+   ('Grinch' ,7, 'M', 'white', 'pessimistic', TRUE),
+   ('Wendy' ,21, 'F', 'brown', 'choleric', FALSE),
+   (NULL ,10, 'M', 'black', 'phalgmatic', TRUE);
